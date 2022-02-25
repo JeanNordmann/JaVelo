427bbc3d6bd8ac43a9d2a2b189170c6fa94232b1
JNordm <1nordmannjean@gmail.com>
2022-02-25 17:58:57 +0100

    2.3.1-2 création coquilles vide de class car flemme de finir enft

---
+++ src/ch/epfl/javelo/projection/PointWebMercator.java
@@ -0,0 +1,19 @@
+package ch.epfl.javelo.projection;
+
+/**
+ * 2.3.2
+ * PointWebMercator
+ *
+ * @author Jean Nordmann (344692)
+ * @author Maxime Ducourau (329544)
+ */
+
+public final class PointWebMercator {
+
+    /**
+     * Constructeur privé, car cette classe n'est pas censée être instantiable.
+     */
+
+    private PointWebMercator() {}
+    //TODO
+}
---
+++ src/ch/epfl/javelo/projection/WebMercator.java
@@ -0,0 +1,56 @@
+package ch.epfl.javelo.projection;
+
+/**
+ * 2.3.1
+ * WebMercator
+ *
+ * (WebMercator ne valident pas leurs arguments, ce travail étant laissé aux classes représentant les points.)
+ *
+ * @author Jean Nordmann (344692)
+ * @author Maxime Ducourau (329544)
+ */
+
+public final class WebMercator {
+
+    /**
+     * Constructeur privé, car cette classe n'est pas censée être instantiable.
+     */
+
+    private WebMercator() {}
+
+    /**
+     *
+     * @param lon
+     * @return la coordonnée x de la projection d'un point se trouvant à la longitude lon, donnée en radians.
+     */
+    public static double x(double lon) {
+        return 0;
+    }
+
+    /**
+     *
+     * @param lat
+     * @return la coordonnée y de la projection d'un point se trouvant à la latitude lat, donnée en radians.
+     */
+    double y(double lat){
+        return 0;
+    }
+
+    /**
+     *
+     * @param x
+     * @return la longitude, en radians, d'un point dont la projection se trouve à la coordonnée x donnée.
+     */
+    double lon(double x){
+        return 0;
+    }
+
+    /**
+     *
+     * @param y
+     * @return la latitude, en radians, d'un point dont la projection se trouve à la coordonnée y donnée.
+     */
+    double lat(double y){
+        return 0;
+    }
+}
