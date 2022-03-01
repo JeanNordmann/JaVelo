f6748ca2a48077999b5ede79e7eee2ce62a7902f
JNordm <1nordmannjean@gmail.com>
2022-03-01 09:44:10 +0100

    bits marche

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -18,11 +18,40 @@ public final class Bits {
 
     private Bits() {}
 
-    /*int extractSigned(int value, int start, int length) {
-        String bit = Integer.toString(value);
-        int leftShift = bit.substring(31 - start - length, 31);
-        leftShift = "hahah";
-    }*/
+    /**
+     * ATTENTION CHECK LE PUBLIC STATIC
+     * qui extrait du vecteur de 32 bits value la plage de length bits commençant au bit d'index start,
+     * qu'elle interprète comme une valeur signée en complément à deux, ou lève une exception si la plage est invalide.
+     * @param value Vecteur de 32 bits initial
+     * @param start Position du bit de début
+     * @param length Longueur à extraire
+     * @return Le bit extrait de façon signée
+     */
+
+    public static int extractSigned(int value, int start, int length) {
+        Preconditions.checkArgument((0 <= start && start <= 31 &&
+                0 <= length && length <= 31 && (start + length) <= 32));
+        int shiftValue = 32 - length - start;
+        return value << shiftValue >> 32 - length;
+
+    }
+
+
+    /**
+     * ATTENTION CHECK LE PUBLIC STATIC
+     * @param value Vecteur de 32 bits initial
+     * @param start Position du bit de début
+     * @param length Longueur à extraire
+     * @return Le bit extrait de façon non signée
+     */
+
+    public static int extractUnsigned(int value, int start, int length) {
+        Preconditions.checkArgument((0 <= start && start <= 31 &&
+                0 <= length && length <= 31 && (start + length) <= 32));
+        int shiftValue = 32 - length - start;       //
+        return value << shiftValue >>> 32 - length;
+    }
+    //qui fait la même chose que la méthode précédente, à deux différences près : d'une part, la valeur extraite est interpretée de manière non signée, et d'autre part l'exception IllegalArgumentException est également levée si length vaut 32.
 
 
 
