74b3c38eb12755cc409b78c71ea0220be283e2a2
JNordm <1nordmannjean@gmail.com>
2022-05-04 23:30:02 +0200

    correction de class final

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -3,14 +3,30 @@ package ch.epfl.javelo.gui;
 import ch.epfl.javelo.routing.ElevationProfile;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.scene.layout.Pane;
 
 public final class ElevationProfileManager {
     private final ReadOnlyObjectProperty<ElevationProfile> elevationProfile;
     private final ReadOnlyDoubleProperty highlightedPosition;
 
+
+    /**
+     *
+     */
+    private final Pane borderPane;
+
     public ElevationProfileManager(ReadOnlyObjectProperty<ElevationProfile> elevationProfile,
                                    ReadOnlyDoubleProperty highlightedPosition) {
         this.elevationProfile = elevationProfile;
         this.highlightedPosition = highlightedPosition;
+        borderPane = new Pane();
+    }
+
+    /**
+     * Méthode publique retournant le panneau de la classe.
+     * @return Le panneau de la classe, de type Pane.
+     */
+    public Pane borderPane() {
+        return borderPane;
     }
 }
